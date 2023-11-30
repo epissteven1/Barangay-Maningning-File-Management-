35de930457a9e7cc04b7caa09f366c7b6c2c0a33
epissteven1 <stevenepis9@gmail.com>
2023-11-30 21:10:48 +0800

    add role to select admin or applicant for login and registration

--- loginrole.php
+++ loginrole.php
@@ -1,133 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#role").change(function() {
+                var selectedRole = $(this).val();
 
-<style>
+                // Use AJAX to load the corresponding login form
+                $.ajax({
+                    type: "POST",
+                    url: "load_login_form.php",
+                    data: { role: selectedRole },
+                    success: function(response) {
+                        $("#loginFormContainer").html(response);
+                    }
+                });
+            });
+        });
+    </script>
+</head>
+<body>
 
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    background-color: #f2f2f2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: 0;
-}
+<form method="post" action="admin_form.php">
+    <label for="role"></label>
+    <select name="role" id="role">
+        <option value="admin">Admin</option>
+        <option value="applicant">Applicant</option>
+    </select>
+    <br>
 
-.login-container {
-    background-color: #f9f9f9;
-    border-radius: 5px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
-    padding: 20px;
-    width: 300px;
-    text-align: center;
-}
-
-h2 {
-    margin: 0;
-    background-color:beige;
-    color: black;
-    border-radius: 5px;
-    padding: 10px;
-    margin-top: -20px;
-    margin-bottom: 20px;
-}
-
-.form-group {
-    margin: 20px 0;
-}
-
-label {
-    display: block;
-    font-weight: bold;
-}
-
-input[type="text"],
-input[type="password"] {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 3px;
-    font-size: 16px;
-}
-
-input[type="submit"] {
-    width: 100%;
-    padding: 10px;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 3px;
-    font-size: 16px;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-}
-
-input[type="submit"]:hover {
-    background-color: #0056b3;
-}
-
-a {
-    color: #007bff;
-    text-decoration: none;
-}
-
-a:hover {
-    text-decoration: underline;
-}
-
-/* Style for the dropdown button */
-.dropdown {
-    position: relative;
-    display: inline-block;
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-}
-
-.dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-.dropdown-content a:hover {
-    background-color: #f1f1f1;
-}
-
-.dropdown:hover .dropdown-content {
-    display: block;
-}
-</style>
-
-
-
-
-<div class="login-container">
-    <h2>Login</h2>
-    <div class="dropdown">
-        <button class="dropbtn">Select Role</button>
-        <div class="dropdown-content">
-            <a href="login.php">Admin</a>
-            <a href="applicant_login.php">Applicant</a>
-        </div>
+    <div id="loginFormContainer">
+        <!-- Login form will be loaded here -->
     </div>
-    <form action="" method="post">
-    <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Login" name="login">
-            </div>
-           <a href="register.php">Don't have an account?</a>
-    </form>
-</div>
+</form>
+
+</body>
+</html>
