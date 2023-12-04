492cc2460f8904040f90b170fdecb4e00c2c4696
Charlynn19 <151125104+Charlynn19@users.noreply.github.com>
2023-12-04 10:07:35 +0800

    Update admin_app_list.php [catulpos]

--- admin_app_list.php
+++ admin_app_list.php
@@ -1,37 +1,184 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
-    <title>Document</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>BMFM SYSTEM</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="template/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="template/vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="template/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="image/Sogod.png" />
 </head>
 <body>
-    <!-- /.row -->
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Applicant Registered</h3>
-
-                    <div class="card-tools">
-                        <form method="get">
-                            <div class="input-group input-group-sm" style="width: 150px;">
-                                <input type="text" name="table_search" class="form-control float-right" placeholder="Search" value="<?php echo isset($_GET['table_search']) ? htmlspecialchars($_GET['table_search']) : ''; ?>">
-
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn btn-default">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a class="navbar-brand brand-logo" href="admindashboard.php">BMFMS</a>
+        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="image/Sogod.png" alt="logo"/></a>
+      </div>
+      <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+          <span class="ti-view-list"></span>
+        </button>
+        <ul class="navbar-nav mr-lg-2">
+          <li class="nav-item nav-search d-none d-lg-block">
+            <div class="input-group">
+              <div class="input-group-prepend hover-cursor" id="navbar-search-icon">
+                <span class="input-group-text" id="search">
+                  <i class="ti-search"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search now" aria-label="search" aria-describedby="search">
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav navbar-nav-right">
+          <li class="nav-item dropdown">
+            <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
+              <i class="ti-bell mx-0"></i>
+              <span class="count"></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="notificationDropdown">
+              <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+              <a class="dropdown-item">
+                <div class="item-thumbnail">
+                  <div class="item-icon bg-success">
+                    <i class="ti-info-alt mx-0"></i>
+                  </div>
                 </div>
-                <!-- /.card-header -->
-                <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
+                <div class="item-content">
+                  <h6 class="font-weight-normal">Application Error</h6>
+                  <p class="font-weight-light small-text mb-0 text-muted">
+                    Just now
+                  </p>
+                </div>
+              </a>
+              <a class="dropdown-item">
+                <div class="item-thumbnail">
+                  <div class="item-icon bg-warning">
+                    <i class="ti-settings mx-0"></i>
+                  </div>
+                </div>
+                <div class="item-content">
+                  <h6 class="font-weight-normal">Settings</h6>
+                  <p class="font-weight-light small-text mb-0 text-muted">
+                    Private message
+                  </p>
+                </div>
+              </a>
+              <a class="dropdown-item">
+                <div class="item-thumbnail">
+                  <div class="item-icon bg-info">
+                    <i class="ti-user mx-0"></i>
+                  </div>
+                </div>
+                <div class="item-content">
+                  <h6 class="font-weight-normal">New user registration</h6>
+                  <p class="font-weight-light small-text mb-0 text-muted">
+                    2 days ago
+                  </p>
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="nav-item nav-profile dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
+              <img src="image/admin.jpg" alt="profile"/>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+              <a class="dropdown-item">
+                <i class="ti-settings text-primary"></i>
+                Settings
+              </a>
+              <a class="dropdown-item">
+                <i class="ti-power-off text-primary"></i>
+                Logout
+              </a>
+            </div>
+          </li>
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="ti-view-list"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="new.php">
+              <i class="ti-shield menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admindashboard.php">
+              <i class="ti-user menu-icon"></i>
+              <span class="menu-title">Registered Users</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_app_list.php">
+              <i class="ti-file menu-icon"></i>
+              <span class="menu-title">Applicants</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_docs.php">
+              <i class="ti-files menu-icon"></i>
+              <span class="menu-title">Documents</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin_activity_log.php">
+              <i class="ti-timer menu-icon"></i>
+              <span class="menu-title">Logs</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="ti-settings menu-icon"></i>
+              <span class="menu-title">Manage Request</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Pending</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Authentication</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="d-flex justify-content-between align-items-center">
+                <div>
+                  <h4 class="font-weight-bold mb-0"> Applicant</h4>
+                </div>
+              </div>
+            </div>
+            <!-- kuwag ug search -->
+          </div>
+          <div class="col-lg-12 stretch-card">
+              <div class="card">
+              <div class="card-body">
+                  <h4 class="card-title">REGISTERED APPLICANT</h4>
+                  <div class="table-responsive">
+                    <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th>APPLICANT_ID</th>
@@ -41,7 +188,7 @@
                                 <th>Action</th>
                             </tr>
                         </thead>
-                        <tbody>
+                      <tbody>
                             <?php
                             include 'connection.php';
 
@@ -60,60 +207,53 @@
                                 echo "<td>{$row['username']}</td>";
                                 echo "<td>{$row['email']}</td>";
                                 echo "<td>
-                                        <a href='edit.php?id={$row['applicant_id']}' class='btn btn-primary btn-sm'>Edit</a>
-                                        <button class='btn btn-danger btn-sm' onclick='deleteApplicant({$row['applicant_id']})'>Delete</button>
+                                        <a href='edit.php?id={$row['applicant_id']}' class='btn btn-primary btn-md'><i class='ti-pencil-alt'></i> Edit</a>
+                                        <button class='btn btn-danger btn-md' onclick='deleteApplicant({$row['applicant_id']})'><i class='ti-trash'></i> Delete</button>
                                       </td>";
                                 echo "</tr>";
                             }
                             ?>
                         </tbody>
                     </table>
+                  </div>
                 </div>
-                <!-- /.card-body -->
+              </div>
             </div>
-            <!-- /.card -->
         </div>
+        
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="#" >bootstrapdash.com </a>2023-2024</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">BMFMS SYSTEM <a href="#" > ADDB </a> projects</span>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
     </div>
-    <!-- jQuery -->
-    <script src="assets/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="assets/dist/js/adminlte.min.js"></script>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
 
-    <script>
-       function deleteApplicant(applicantId) {
-    if (confirm('Are you sure you want to delete this applicant?')) {
-        // Use the applicantId in the URL for the window.location.href
-        window.location.href = 'delete_applicant.php?id=' + applicantId;
-
-        // Use the applicantId in the data for the AJAX request
-        $.ajax({
-            type: 'GET',
-            url: 'delete_applicant.php',
-            data: { id: applicantId },
-            success: function(response) { 
-              
-                var data = JSON.parse(response);
-                console.log(data); // Add this line to log the response
-                console.log(applicantId);
-
-                if (data.success) {
-                    // Use the applicantId in the redirect URL
-                    window.location.href = 'admin_index.php';
-                } else {
-                    alert('Failed to delete applicant.');
-                }
-            },
-            error: function(xhr, status, error) {
-                console.error('AJAX Error:', status, error);
-            }
-            
-        });
-    }
-}
-
-
-    </script>
+  <!-- plugins:js -->
+  <script src="template/vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="template/vendors/chart.js/Chart.min.js"></script>
+  <script src="template/js/jquery.cookie.js" type="text/javascript"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="template/js/off-canvas.js"></script>
+  <script src="template/js/hoverable-collapse.js"></script>
+  <script src="template/js/template.js"></script>
+  <script src="template/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="template/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+  <script src="template/js/file-upload.js"></script>
 </body>
+
 </html>
+
